2679964c7c5d2027e317dffb44cc2e370954709f
Xingyu Li <xingyu.li@levo.legal>
2021-04-08 10:32:32 +0800

    add basic seo css config

---
+++ src/components/animations/CoverEaseInOut.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CoverEaseInOut = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CoverEaseInOut
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,11 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import SEO from 'components/layout/SEO'
 
 export default function Home() {
   return (
     <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
+      <SEO title='Home page'/>
       <main>
         <h1 className="title">
           Welcome to <Link href="/more">Next.js!</Link>
